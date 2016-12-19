5943f8c1575853bfbf9b7822abd87f8f10bb4cbc
Muhammad Zaky Zulfiqor <zakybstrd21215@users.noreply.github.com>
2016-12-19 21:45:40 +0700

    Update hello.py

--- src/hello.py
+++ src/hello.py
@@ -1,4 +1,1 @@
-#Hello World Program from Priyank gada - Python for Hackers tutorial series
-#www.youtube.com/priyankgada
-
-print " Hello World ! , Please visit www.youtube.com/priyankgada "
+print " Hello World ! , Please visit https://errorcybernews.com "
